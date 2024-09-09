d7640e2caa047a64da3c08e6153a5a743a80ddfd
nicoplacente <nicolasplacente@gmail.com>
2024-09-09 10:15:16 -0300

    update validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,22 @@ export const dynamic = "force-dynamic";
 export default async function Home() {
   const allTasks = await prisma.task.findMany();
 
+  if (allTasks.length !== 0) {
+    return (
+      <main>
+        <SectionContainer>
+          <section className="flex flex-wrap max-w-[90%] w-full justify-center gap-12">
+            {allTasks.map((task) => (
+              <TasksCard task={task} key={task.id} />
+            ))}
+          </section>
+        </SectionContainer>
+      </main>
+    );
+  }
   return (
-    <main>
-      <SectionContainer>
-        <section className="flex flex-wrap max-w-[90%] w-full justify-center gap-12">
-          {allTasks.map((task) => (
-            <TasksCard task={task} key={task.id} />
-          ))}
-        </section>
-      </SectionContainer>
-    </main>
+    <SectionContainer>
+      <p className="text-sm font-semibold">There are no pending tasks</p>
+    </SectionContainer>
   );
 }
