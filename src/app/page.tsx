0e8def1e5e54dcc3bad8c78268952e4a723b1e18
nicoplacente <nicolasplacente@gmail.com>
2024-09-09 03:32:50 -0300

    update task card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import SectionContainer from "@/components/section-container";
 import TasksCard from "@/components/tasks-card";
+import prisma from "@/utils/prisma";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
+  const allTasks = await prisma.task.findMany();
+
   return (
     <main>
       <SectionContainer>
-        <TasksCard />
+        <section className="flex flex-wrap max-w-[90%] w-full justify-center gap-12">
+          {allTasks.map((task) => (
+            <TasksCard task={task} key={task.id} />
+          ))}
+        </section>
       </SectionContainer>
     </main>
   );
